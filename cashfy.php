2465677ebc6e2c632e3cc46f2b22d6837347a46a
Andrei Freire de Almeida <andreifalmeida08@gmail.com>
2026-09-02 20:59:18 -0400

    Correção simples de arquivos desnecessários ou duplicados

--- cashfy.php
+++
@@ -1,9 +0,0 @@
-<?php
-function conn(){
-    $conn = mysqli_connect('localhost', 'root', '', 'cashfy');
-    if(!$conn){
-        die("Erro de conexão" . mysqli_connect_error());
-    }
-    return $conn; 
-}
-?>
